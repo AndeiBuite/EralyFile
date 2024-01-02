becb5ffbdb3c79e0a87e5d4248cf73ce4d997e65
Andei_Buite <Andei.Buite@outlook.com>
2024-01-02 14:28:44 +0800

    Update 路径长度配置

--- basic/src/main/kotlin/io/github/andeibuite/eraly/basic/metadata/EralyBasicConfig.kt
+++ basic/src/main/kotlin/io/github/andeibuite/eraly/basic/metadata/EralyBasicConfig.kt
@@ -2,5 +2,11 @@ package io.github.andeibuite.eraly.basic.metadata
 
 object EralyBasicConfig
 {
+	// 路径相关配置
+	object Path
+	{
+		// Max char number to path expression
+		val maxLength: UInt = 1024u * 4u
+	}
 
 }
